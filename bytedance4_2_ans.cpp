43ae83005a2f822b8e6931691d261b41638995ca
dmmlcloud <41765012+dmmlcloud@users.noreply.github.com>
2019-04-12 10:08:00 +0800

    Update bytedance4_2_ans.cpp

--- bytedance4_2_ans.cpp
+++ bytedance4_2_ans.cpp
@@ -6,27 +6,10 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int take = -1;
         int profit = 0;
-        if(prices.empty() || prices.size() == 1)
-            return profit;
-        for(int i = 0; i < prices.size(); i++){
-            if(take == -1){
-                if(i+1 == prices.size())
-                    break;
-                else if(prices[i] >= prices[i+1])
-                    continue;
-                else
-                    take = prices[i];
-            }else{
-                if(i+1 == prices.size())
-                    profit = profit + prices[i]-take;
-                else if(prices[i] < prices[i+1])
-                    continue;
-                else if(prices[i] >= prices[i+1]){
-                    profit = profit + prices[i] - take;
-                    take = -1;
-                }
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] >= prices[i-1]){
+                profit = profit + prices[i] - prices[i-1];
             }
         }
         return profit;
